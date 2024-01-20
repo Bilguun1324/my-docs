065d60133070c365c21003308f59ab9ec303e658
Bilguun <bilguun1324@gmail.com>
2024-01-20 16:25:36 +1100

    updated firebase config

--- my-docs-front-end/src/firebase/firebase.ts
+++ my-docs-front-end/src/firebase/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSENGER_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: "AIzaSyBs-4wUe7aFwPDa0s-v8RA6pgLowr0ry_o",
+  authDomain: "my-docs-5c524.firebaseapp.com",
+  projectId: "my-docs-5c524",
+  storageBucket: "my-docs-5c524.appspot.com",
+  messagingSenderId: "1089221895101",
+  appId: "1:1089221895101:web:66b3bcf8e20cdbb8d868d4",
+  measurementId: "G-EKX84B3CGT"
 };
 
 const app = initializeApp(firebaseConfig);
