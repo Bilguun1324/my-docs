f2591e74075cf7a312030808e6876785e3c45db2
Bilguun <bilguun1324@gmail.com>
2024-02-07 17:44:19 +1100

    Added Animations on Navigation

--- my-docs-front-end/src/components/menu-bar.tsx
+++ my-docs-front-end/src/components/menu-bar.tsx
@@ -2,26 +2,26 @@ import { motion } from "framer-motion";
 
 type MenuBarType = {
   open?: boolean;
-  setOpen?: (open: boolean) => void;
+  setOpen?: () => void;
 };
 
 export const MenuBar = (props: MenuBarType) => {
-  const { open = true, setOpen } = props;
+  const { open = false, setOpen } = props;
 
   return (
     <div
-      className={`h-12 w-12 flex ${
+      className={`h-9 w-9 flex ${
         open ? "justify-center items-center" : "flex-col justify-evenly"
       } cursor-pointer relative`}
-      onClick={() => setOpen}
+      onClick={setOpen}
     >
       <motion.div
         animate={{ rotate: open ? 45 : 0 }}
-        className="bg-white h-1 w-8 absolute"
+        className={`bg-white h-1 w-8 ${open && "absolute"} rounded`}
       ></motion.div>
       <motion.div
         animate={{ rotate: open ? -45 : 0 }}
-        className="bg-white h-1 w-8 absolute"
+        className={`bg-white h-1 w-8 ${open && "absolute"} rounded`}
       ></motion.div>
     </div>
   );
--- my-docs-front-end/src/utils/sizes.tsx
+++ my-docs-front-end/src/utils/sizes.tsx
@@ -1,3 +1,5 @@
 export const inputWidth = 72;
-export const mediumWidth = 64;
-export const smallWidth = 16;
+export const largeWidth = 312;
+export const mediumWidth = 256;
+export const smallWidth = 72;
+export const extraSmallWidth = 12;
